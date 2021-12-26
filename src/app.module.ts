2eec50edc4cc75b5eecd694849becaacf432f6fb
Ricardo Morato <rmr@cin.ufpe.br>
2021-12-26 16:57:39 -0300

    chore: handle mongodb connection on app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_URI),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
