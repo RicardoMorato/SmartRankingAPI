ac709a27d29ba913965901baca42b0830834b1bb
Ricardo Morato <rmr@cin.ufpe.br>
2021-12-28 22:13:11 -0300

    feat: create main files for entity "categorias"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION_URI),
     JogadoresModule,
+    CategoriasModule,
   ],
   controllers: [],
   providers: [],
