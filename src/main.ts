24acfd434b7c7870a34458f0588aa45132a61889
Ricardo Morato <rmr@cin.ufpe.br>
2021-12-29 00:39:41 -0300

    feat: add global http exception handler to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionsFilter } from './common/filters/http-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionsFilter());
   await app.listen(8080);
 }
 bootstrap();
