43ece4f7bba17d7f405be2e69f6fbb1d6b153937
Ricardo Morato <rmr@cin.ufpe.br>
2021-12-28 23:16:33 -0300

    feat: handling updating "categorias"

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -4,10 +4,12 @@ import {
   Get,
   Param,
   Post,
+  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria.dto';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 import { Categoria } from './interfaces/categoria.interface';
 
@@ -33,5 +35,16 @@ export class CategoriasController {
     @Param('categoria') categoria: string,
   ): Promise<Categoria> {
     return await this.categoriasService.consultarCategoriaPorNome(categoria);
+  }
+
+  @Put('/categoria')
+  async atualizarCategoria(
+    @Param('categoria') categoria: string,
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+  ): Promise<void> {
+    await this.categoriasService.atualizarCategoria(
+      categoria,
+      atualizarCategoriaDto,
+    );
   }
 }
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,15 +1,19 @@
 import {
   BadRequestException,
   Injectable,
+  Logger,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria.dto';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 import { Categoria } from './interfaces/categoria.interface';
 
 @Injectable()
 export class CategoriasService {
+  private readonly logger = new Logger(CategoriasService.name);
+
   constructor(
     @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>,
   ) {}
@@ -21,15 +25,18 @@ export class CategoriasService {
 
     const categoriaEncontrada = await this._buscarCategoriaPorNome(categoria);
 
-    if (categoriaEncontrada) {
+    if (!!categoriaEncontrada) {
       throw new BadRequestException(
         `A categoria ${categoria} já foi cadastrada previamente`,
       );
     }
 
     const categoriaCriada = new this.categoriaModel(criarCategoriaDto);
+    await categoriaCriada.save();
 
-    return await categoriaCriada.save();
+    this.logger.log(`Categoria criada: ${categoriaCriada._id}`);
+
+    return categoriaCriada;
   }
 
   async consultarTodasCategorias(): Promise<Categoria[]> {
@@ -48,7 +55,31 @@ export class CategoriasService {
     return categoriaEncontrada;
   }
 
-  async _buscarCategoriaPorNome(categoria: string): Promise<Categoria> {
+  async atualizarCategoria(
+    categoria: string,
+    atualizarCategoriaDto: AtualizarCategoriaDto,
+  ): Promise<void> {
+    const categoriaEncontrada = await this._buscarCategoriaPorNome(categoria);
+
+    if (!!categoriaEncontrada) {
+      const categoriaAtualizada = await this.categoriaModel.findOneAndUpdate(
+        { categoria },
+        atualizarCategoriaDto,
+      );
+
+      this.logger.log(`Categoria atualizada: ${categoriaAtualizada._id}`);
+    } else {
+      this.logger.warn(
+        `Falha ao atualizar categoria ${categoria}, categoria não encontrada`,
+      );
+
+      throw new NotFoundException(
+        `Falha ao atualizar categoria ${categoria}, categoria não encontrada`,
+      );
+    }
+  }
+
+  private async _buscarCategoriaPorNome(categoria: string): Promise<Categoria> {
     return await this.categoriaModel
       .findOne({
         categoria,
