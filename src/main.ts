60c20b77e76e5c9352d7f8ae19023595bd5de717
Ricardo Morato <rmr@cin.ufpe.br>
2021-12-29 23:49:54 -0300

    feat: override Date.toJSON on main to handle the correct timezone

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as momentTimezone from 'moment-timezone';
 import { AppModule } from './app.module';
 import { HttpExceptionsFilter } from './common/filters/http-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionsFilter());
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
   await app.listen(8080);
 }
 bootstrap();
